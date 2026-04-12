ccb824be60a7d25a8d9786a44e3c7f56e819c610
Alakok77 <top.p1234558@gmail.com>
2026-04-12 11:07:57 +0700

    chore: remove basePath for root vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/Game-Demo",
   trailingSlash: true,
   images: {
     unoptimized: true,
